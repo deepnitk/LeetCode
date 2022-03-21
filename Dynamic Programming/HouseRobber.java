103d7d8a3ca506e2d403f18d03ba2970848346eb
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-03-21 14:04:53 +0530

    Update HouseRobber.java

--- Dynamic Programming/HouseRobber.java
+++ Dynamic Programming/HouseRobber.java
@@ -21,3 +21,30 @@ class Solution {
         return dp[idx] = Math.max(pick, notPick);
     }
 }
+
+//Tabulization
+//TC:O(N) SC:O(N)
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        //step 1: Declare dp and pass
+        int[] dp = new int[n];
+        Arrays.fill(dp,-1);
+        return helper(n, nums, dp);
+    }
+    
+    private int helper(int n, int[] nums, int[] dp){
+        dp[0] = nums[0];
+        int neg = 0;
+        for(int i=1;i<n;i++){
+            int pick = nums[i];
+            if(i>1)
+                pick+=dp[i-2];
+            int notPick = 0 + dp[i-1];
+            dp[i] = Math.max(pick, notPick);
+        }
+        return dp[n-1];
+    }
+}
+
+
