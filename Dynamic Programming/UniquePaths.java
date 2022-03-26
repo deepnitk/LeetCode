15e6fa5eb8709a419759eb3b0290935bc30f2517
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-03-26 18:09:07 +0530

    Update UniquePaths.java

--- Dynamic Programming/UniquePaths.java
+++ Dynamic Programming/UniquePaths.java
@@ -37,3 +37,35 @@ class Solution {
         return dp[i][j] =top+left;
     }
 }
+
+//Tabulation solution
+//TC: O(M*N)
+//SC: O(M*N)
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        return uniquePathUtils(m, n);
+    }
+    
+    private int uniquePathUtils(int m, int n){
+        int[][] dp = new int[m][n];
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+        
+        for(int i=0;i<m;i++){
+            for(int j=0; j<n;j++){
+                //base condition
+                  if(i==0 && j==0){
+                      dp[i][j]=1;
+                      continue;
+                  }
+                int top = 0;
+                int left = 0;
+                if(i>0) top = dp[i-1][j];
+                if(j>0) left = dp[i][j-1];
+                dp[i][j] = top + left;
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
