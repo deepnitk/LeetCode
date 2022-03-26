89b937dc5a68c58a2a6997f7787452acec0ab4b0
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-03-26 15:20:42 +0530

    Update UniquePaths.java
    There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
    
    Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
    
    The test cases are generated so that the answer will be less than or equal to 2 * 109.

--- Dynamic Programming/UniquePaths.java
+++ Dynamic Programming/UniquePaths.java
@@ -13,3 +13,24 @@ class Solution {
         return top+left;
     }
 }
+
+//Memoization
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m+1][n+1];
+        for(int i=0;i<m;i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return uniquePathUtils(m-1, n-1, dp);
+    }
+    
+    private int uniquePathUtils(int i, int j, int[][] dp){
+        if(i == 0 && j == 0) return 1;
+        if(i < 0 || j < 0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        int top = uniquePathUtils(i-1, j, dp);
+        int left = uniquePathUtils(i, j-1, dp);
+        return dp[i][j] =top+left;
+    }
+}
