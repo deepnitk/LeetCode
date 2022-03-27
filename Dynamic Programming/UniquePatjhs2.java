c7087987eaa09f8337c5a75e9bae9c42f88fb572
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-03-27 17:54:30 +0530

    Update UniquePatjhs2.java
    tabulation solution

--- Dynamic Programming/UniquePatjhs2.java
+++ Dynamic Programming/UniquePatjhs2.java
@@ -22,3 +22,39 @@ class Solution {
         return dp[i][j] =top+left;
     }
 }
+
+//Tabulation
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        
+        return uniquePathUtils(m, n, obstacleGrid);
+    }
+    
+    private int uniquePathUtils(int m, int n, int[][] obstacleGrid){
+        int[][] dp = new int[m][n];
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+        
+        for(int i=0;i<m;i++){
+            for(int j=0; j<n;j++){
+                //base condition
+                  if(i==0 && j==0){
+                      dp[i][j]=1;
+                      continue;
+                  }
+                if(obstacleGrid[i][j] == 1) {
+                    dp[i][j]=0;
+                    continue;
+                }
+                int top = 0;
+                int left = 0;
+                if(i>0) top = dp[i-1][j];
+                if(j>0) left = dp[i][j-1];
+                dp[i][j] = top + left;
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
