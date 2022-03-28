7e8ca111bcafcfb6cfd2d7096df11508260dca8e
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-03-28 08:21:27 +0530

    Update MinSumPath.java
    Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
    
    Note: You can only move either down or right at any point in time.

--- Dynamic Programming/MinSumPath.java
+++ Dynamic Programming/MinSumPath.java
@@ -23,3 +23,36 @@ class Solution {
         return dp[i][j] = Math.min(up, left);
     }
 }
+
+//Tabulation solution
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        
+        return minPathSumUtils(m, n, grid, dp);
+    }
+    
+    private int minPathSumUtils(int m, int n, int[][] grid, int[][] dp){
+        
+        for(int i =0; i<m;i++){
+            for(int j=0;j< n; j++){
+                if(i==0 && j==0) dp[i][j] = grid[i][j];
+                else{
+                    int up = grid[i][j];
+                    if(i>0) up += dp[i-1][j];
+                    else up += (int)Math.pow(10,9);
+
+                    int left = grid[i][j];
+                    if(j>0) left+=dp[i][j-1];
+                    else left += (int)Math.pow(10,9);
+                    
+                    dp[i][j] = Math.min(up, left);
+                }
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+}
