748befe8fa366ebece86833dd85ca004236c383f
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-03-28 18:05:11 +0530

    Update Triangle.java

--- Dynamic Programming/Triangle.java
+++ Dynamic Programming/Triangle.java
@@ -16,3 +16,27 @@ class Solution {
     }
     
 }
+
+//Memoization
+//TC: O(N*N)
+//SC: O(N*N) + O(N)
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int m = triangle.size();
+        int n = triangle.get(0).size();
+        int[][] dp = new int[m][m];
+        for(int[] arr: dp)
+            Arrays.fill(arr, -1);
+        return minimumTotalUtil(0,0,triangle, m, dp);
+    }
+    
+    private int minimumTotalUtil(int i,int j, List<List<Integer>> triangle, int m, int[][] dp) {
+      //Base case
+        if(i==m-1) return triangle.get(m-1).get(j);
+        if(dp[i][j] != -1) return dp[i][j];
+        int down = triangle.get(i).get(j) + minimumTotalUtil(i+1, j, triangle, m, dp);
+        int diag = triangle.get(i).get(j) + minimumTotalUtil(i+1, j+1, triangle, m, dp);
+        return dp[i][j] = Math.min(down, diag);
+    }
+    
+}
