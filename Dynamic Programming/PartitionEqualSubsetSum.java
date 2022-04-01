5e27e5f71ccdd4ea2bef7921704b26683218500b
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-04-01 09:20:09 +0530

    Update PartitionEqualSubsetSum.java

--- Dynamic Programming/PartitionEqualSubsetSum.java
+++ Dynamic Programming/PartitionEqualSubsetSum.java
@@ -1,3 +1,4 @@
+//Recursive solution
 //TC: O(2^N)
 //SC: O(2^N) + O(K)
 
@@ -22,5 +23,36 @@ class Solution {
             pick = canPartitionUtil(idx-1, k-nums[idx], nums);
         
         return (pick || notPick);
+    }
+}
+
+//Memoization solution
+
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int k = 0;
+        for(int a:nums)
+            k+=a;
+        boolean[][] dp = new boolean[nums.length][k/2 +1];
+        for(boolean[] row:dp)
+            Arrays.fill(row, false);
+        if(k%2 != 0) return false;
+        return canPartitionUtil(nums.length-1, k/2, nums, dp);
+    }
+    
+    private boolean canPartitionUtil(int idx, int k, int[] nums, boolean[][] dp){
+        if(k==0) return true;
+        if(idx == 0){
+            if(nums[0]== k) return true;
+            else return false;
+        }
+        if(dp[idx][k] != false) return dp[idx][k];
+        
+        boolean notPick = canPartitionUtil(idx-1, k, nums, dp);
+        boolean pick = false;
+        if(k>=nums[idx])
+            pick = canPartitionUtil(idx-1, k-nums[idx], nums, dp);
+        
+        return dp[idx][k] = (pick || notPick);
     }
 }
