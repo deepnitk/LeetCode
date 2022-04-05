f2b42e48b95b911f523d7d3b58375ec72fb0b199
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-04-05 09:10:38 +0530

    Update TargetSum.java

--- Dynamic Programming/TargetSum.java
+++ Dynamic Programming/TargetSum.java
@@ -36,3 +36,48 @@ class Solution {
     }
     
 }
+
+//Memoization
+// TC: O(N*SUM)
+// SC: O(N*SUM) + O(N)
+
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int totalSum = 0;
+        int s = 0;
+        int n = nums.length;
+        for(int num:nums)
+            totalSum+=num;
+        
+        //Checking for edge cases
+        if(totalSum-target<0) return 0;
+        if((totalSum-target)%2==1) return 0;
+        
+        s = (totalSum-target)/2;
+        
+        int[][] dp = new int[n][s+1];
+        for(int[] row:dp)
+            Arrays.fill(row, -1);
+        
+        return findTargetSumWaysUtils(n-1, nums, s, dp);
+    }
+    
+    private int findTargetSumWaysUtils(int idx, int[] nums, int s, int[][] dp){
+        //Base cases
+        if(idx == 0){
+            if(s==0 && nums[0]==0)
+                return 2;
+            if(s==0 || s == nums[0])
+                return 1;
+            return 0;
+        }
+        if(dp[idx][s] != -1) return dp[idx][s];
+        int notPick = findTargetSumWaysUtils(idx-1, nums, s, dp);
+        int pick = 0;
+        if(nums[idx]<=s)
+            pick += findTargetSumWaysUtils(idx-1, nums, s-nums[idx], dp);
+        
+        return dp[idx][s] = pick + notPick;
+    }
+    
+}
