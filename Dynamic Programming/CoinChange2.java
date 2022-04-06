aefa697d959846060f121f87244a8a3560c7292d
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-04-06 09:34:38 +0530

    Update CoinChange2.java

--- Dynamic Programming/CoinChange2.java
+++ Dynamic Programming/CoinChange2.java
@@ -49,3 +49,39 @@ class Solution {
         return dp[idx][amount] = pick+notPick;
     }
 }
+
+//Tabulation solution
+//TC: O(N*T)
+//SC: O(N*T)
+class Solution {
+    public int change(int amount, int[] coins) {
+
+        
+        return changeUtils(coins, amount);
+    }
+    
+    private int changeUtils(int[] coins, int amount){
+        
+        int n = coins.length;
+        int[][] dp = new int[n][amount+1];
+        for(int[] row:dp)
+            Arrays.fill(row, 0);
+        
+        for(int i=0;i<=amount;i++){
+            if(i%coins[0] == 0) dp[0][i] = 1;
+            else dp[0][i] = 0;
+        }
+        
+        for(int idx=1;idx<n;idx++){
+            for(int amt=0;amt<=amount;amt++){
+                    int notPick = dp[idx-1][amt];
+                    int pick = 0;
+                    if(coins[idx]<=amt) pick += dp[idx][amt-coins[idx]];
+
+                    dp[idx][amt] = pick+notPick;
+            }
+        }
+        
+        return dp[n-1][amount];
+    }
+}
