03be5f1d5e13fd9d73e127e3e33799c423da67cf
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-04-08 16:12:47 +0530

    Create LongestPaliindromicSubsequences.java
    Given a string s, find the longest palindromic subsequence's length in s.
    
    A subsequence is a sequence that can be derived from another sequence by deleting some or no elements without changing the order of the remaining elements.

---
+++ Dynamic Programming/LongestPaliindromicSubsequences.java
@@ -0,0 +1,38 @@
+//Tabulation Solution
+
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        String t = s;
+        String ss=new StringBuilder(s).reverse().toString();
+        return longestCommonSubsequenceUtil(ss,t);
+    }
+    
+    private int longestCommonSubsequenceUtil(String text1, String text2){
+        int n = text1.length();
+        int m = text2.length();
+        //Index shifting to accomodate base case...
+        int[][] dp = new int[n+1][m+1];
+        for(int[] row: dp)
+            Arrays.fill(row, -1);
+
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 0;
+        }
+        for(int i=0;i<=m;i++){
+            dp[0][i] = 0;
+        }
+        
+        for(int idx1= 1;idx1<=n;idx1++){
+            for(int idx2=1;idx2<=m;idx2++){
+                //character match
+                if(text1.charAt(idx1-1) == text2.charAt(idx2-1)) 
+                    dp[idx1][idx2] = 1 + dp[idx1-1][idx2-1];
+
+                //character mismatch
+                else
+                    dp[idx1][idx2] = 0 + Math.max(dp[idx1-1][idx2], dp[idx1][idx2-1]);
+            }
+        }
+        return dp[n][m];  
+    }
+}
