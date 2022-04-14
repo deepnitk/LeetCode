144f368f2416d00445fa5e1e2597d116ae4a190e
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-04-14 12:51:14 +0530

    Update EditDistance.java

--- Dynamic Programming/EditDistance.java
+++ Dynamic Programming/EditDistance.java
@@ -55,3 +55,45 @@ class Solution {
         }
     }
 }
+
+//Tabulation solution
+//TC:O(N*M)
+//SC:O(N*M)
+class Solution {
+    public int minDistance(String word1, String word2) {
+        
+        return minDistanceUtils(word1, word2);
+    }
+    
+    private int minDistanceUtils(String s1, String s2){
+        
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int[] row:dp)
+            Arrays.fill(row, 0);
+        
+        for(int i=0;i<=n;i++)
+            dp[i][0] = i;
+        for(int j=0;j<=m;j++)
+            dp[0][j] =j;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1)) 
+                    dp[i][j] = 0 + dp[i-1][j-1];
+                else{
+                    dp[i][j] = Math.min(
+                        1 + dp[i-1][j],
+                        Math.min(
+                            1 + dp[i][j-1],
+                            1 + dp[i-1][j-1]
+                        )
+                    );
+                }                
+            }
+        }
+        return dp[n][m];
+        
+    }
+}
