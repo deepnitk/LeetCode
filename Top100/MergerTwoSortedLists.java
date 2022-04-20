f7b44a734e6c6c61781e5a18a0aa9497624ccfb3
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-04-20 10:03:42 +0530

    Update MergerTwoSortedLists.java

--- Top100/MergerTwoSortedLists.java
+++ Top100/MergerTwoSortedLists.java
@@ -11,33 +11,22 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         
-        if(list1 == null) return list2;
-        if(list2 == null) return list1;
         ListNode temp1 = list1;
         ListNode temp2 = list2;
-        ListNode sorted;
-        if(temp1.val<=temp2.val){
-            sorted = new ListNode(temp1.val, null);
-            temp1=temp1.next;
-        }
-        else{
-            sorted = new ListNode(temp2.val, null);
-            temp2=temp2.next;
-        }
+        ListNode sorted = new ListNode(0, null);
         ListNode head = sorted;
         
 
         while(temp1!= null && temp2!=null){
             if(temp1.val<=temp2.val){
                 sorted.next = temp1;
-                sorted=sorted.next;
                 temp1 = temp1.next;
             }
             else{
                 sorted.next = temp2;
-                sorted=sorted.next;
                 temp2= temp2.next;
             }
+            sorted=sorted.next;
         }
         
         if(temp1!=null)
@@ -45,7 +34,7 @@ class Solution {
         if(temp2!=null)
             sorted.next = temp2;
         
-        return head;
+        return head.next;
         
       
     }
