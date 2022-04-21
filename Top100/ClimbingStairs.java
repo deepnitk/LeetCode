f1fa0696e7376d73dd4700b5cf4d6bd144df785b
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-04-21 09:24:51 +0530

    Update ClimbingStairs.java

--- Top100/ClimbingStairs.java
+++ Top100/ClimbingStairs.java
@@ -14,3 +14,29 @@ class Solution {
         return climStairsUtils(n-1, dp) + climStairsUtils(n-2, dp);
     }
 }
+
+//Tabulation
+//TC:O(N)
+//SC:O(N)
+class Solution {
+    public int climbStairs(int n) {
+        
+        return climStairsUtils(n);
+    }
+    
+    private int climStairsUtils(int n){
+        //Cornet Case: if n <= 2
+        if(n<=2) return n;
+        
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, 0);
+        
+        for(int i=0;i<=2;i++)
+            dp[i]=i;
+
+        for(int i=3;i<=n;i++)
+            dp[i] = dp[i-1] + dp[i-2];
+        
+        return dp[n];
+    }
+}
