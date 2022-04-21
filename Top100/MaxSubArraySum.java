4b4ea23de742ccae8822c97160522e35f625d583
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-04-21 08:56:57 +0530

    Update MaxSubArraySum.java

--- Top100/MaxSubArraySum.java
+++ Top100/MaxSubArraySum.java
@@ -13,3 +13,18 @@ class Solution {
         return maxSum;
     }
 }
+
+//Kadane's Algo
+//TC:O(N)
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int maxSum = nums[0];
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            maxSum=Math.max(maxSum,sum);
+            if(sum<0) sum = 0;
+        }
+        return maxSum;
+    }
+}
