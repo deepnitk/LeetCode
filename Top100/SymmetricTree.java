f65272aca2eb5568c59bffc492f4fa4da1d50d85
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-04-22 08:08:26 +0530

    Update SymmetricTree.java

--- Top100/SymmetricTree.java
+++ Top100/SymmetricTree.java
@@ -31,3 +31,18 @@ class Solution {
         
     }
 }
+//Better code
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return root == null || isSymmetricUtil(root.left , root.right);  
+    }
+    
+    private boolean isSymmetricUtil(TreeNode root1, TreeNode root2){
+        if(root1 == null || root2 == null)
+            return root1==root2;
+        if(root1.val!=root2.val)
+            return false;
+        return isSymmetricUtil(root1.left, root2.right) && isSymmetricUtil(root1.right, root2.left);
+
+    }
+}
