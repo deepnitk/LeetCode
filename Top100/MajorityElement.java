57bd906ed2fa9d0261df3f153d39e9abafa03430
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-04-24 10:46:04 +0530

    Update MajorityElement.java
    Moore voting Algo

--- Top100/MajorityElement.java
+++ Top100/MajorityElement.java
@@ -16,3 +16,19 @@ class Solution {
         return 0;
     }
 }
+
+//Moore's Voting Algo
+//TC:O(N)
+//SC:O(1)
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+        for(int num:nums){
+            if(count == 0) candidate = num;
+            else if(candidate == num) count++;
+            else count--;
+        }
+        return candidate;
+    }
+}
