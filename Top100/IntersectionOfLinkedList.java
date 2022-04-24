5a1cd67452a0bb76d5fa3557afeebb1e73972815
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-04-24 08:36:38 +0530

    Update IntersectionOfLinkedList.java

--- Top100/IntersectionOfLinkedList.java
+++ Top100/IntersectionOfLinkedList.java
@@ -30,3 +30,55 @@ public class Solution {
         return null;
     }
 }
+
+//Dummy node at both head, in longer LL MOVE dummy node to l1-l2 pos
+//TC:O(2M)
+//SC:O(1)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int l1 = 0;
+        ListNode temp1 = headA;
+        while(temp1 != null){
+           l1++;
+            temp1= temp1.next;
+        }
+        temp1 = headA;
+        int l2 = 0;
+        ListNode temp2 = headB;
+        while(temp2!=null){
+            l2++;
+            temp2=temp2.next;
+        }
+        temp2 = headB;
+        int initialShift = Math.abs(l1-l2);
+        if(l1>l2){
+            while(initialShift > 0){
+                temp1=temp1.next;
+                initialShift--;
+            }
+        }
+        else {
+            while(initialShift > 0){
+                temp2=temp2.next;
+                initialShift--;
+            }
+        }
+        while(temp1!=null){
+            if(temp1==temp2) return temp1;
+            temp1=temp1.next;
+            temp2=temp2.next;
+        }
+        return null;
+    }
+}
