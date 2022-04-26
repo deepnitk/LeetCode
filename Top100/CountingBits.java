e72de81093dbcd2b14bad5b09584a13c42424ebd
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-04-26 17:10:14 +0530

    Update CountingBits.java

--- Top100/CountingBits.java
+++ Top100/CountingBits.java
@@ -18,3 +18,20 @@ class Solution {
         return count;
     }
 }
+
+//TC:O(N)
+//Right shift by 1
+class Solution {
+    public int[] countBits(int n) {
+        int[] res = new int[n+1];
+        res[0] = 0;
+        for(int i=1;i<=n;i++){
+            // here i/2 means right shift by 1 bit. 
+            // for e.g noOfSetBits of 5(101) = noOfSetBits(5/2) + 5%2 = 1+1 = 2
+            // noOfSetBits of 6(110) = noOfSetBits of(6/2) + 6%2 = 2 + 0 = 2 
+            res[i]=res[i/2]+(i%2);
+        }
+        return res;
+    }
+}
+
