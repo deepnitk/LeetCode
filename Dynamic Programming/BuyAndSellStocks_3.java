a53c613cad591c3889f83128fa20b9f104ad5d00
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-04-27 10:27:30 +0530

    Update BuyAndSellStocks_3.java

--- Dynamic Programming/BuyAndSellStocks_3.java
+++ Dynamic Programming/BuyAndSellStocks_3.java
@@ -114,3 +114,45 @@ class Solution {
 
     }
 }
+//Space Opmitimization
+//TC:O(2*3)
+//SC:O(2*3)
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        return maxProfitUtil(prices);
+    }
+    
+    private int maxProfitUtil(int[] prices){
+        
+        int n = prices.length;
+        int[][] after = new int[2][3];
+        for (int[] row : after) {
+            Arrays.fill(row, 0);
+        }
+        int[][] curr = new int[2][3];
+        for (int[] row : curr) {
+            Arrays.fill(row, 0);
+        }
+
+        //Base cases are not needed as we are already initializing with 0
+        
+        for(int idx = n-1;idx>=0;idx--){
+            for(int buy =0;buy<=1;buy++){
+                for(int cap=1;cap<=2;cap++){
+                    if(buy==1){
+                        curr[buy][cap] = Math.max(-prices[idx] + after[0][cap],
+                                                    0 + after[1][cap]);
+                    }
+                    else {
+                        curr[buy][cap] = Math.max(prices[idx] + after[1][cap-1],
+                                                        0 + after[0][cap]);
+                    }
+                }
+            }
+            after = curr;
+        }
+        return after[1][2];
+
+    }
+}
