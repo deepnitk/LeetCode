417b48e9e9238dce3cd737a00f64056cb5e6d382
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-04-28 11:53:12 +0530

    Update LongestSubstringWithoutRepeatingCharacters.java

--- Top100/LongestSubstringWithoutRepeatingCharacters.java
+++ Top100/LongestSubstringWithoutRepeatingCharacters.java
@@ -21,3 +21,25 @@ class Solution {
         return maxi;
     }
 }
+
+//Better approach
+//TC:O(N^2)
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n =s.length();
+        int maxi = Integer.MIN_VALUE;
+        for(int i = 0;i<n;i++){
+            HashSet<Character> hs = new HashSet<>();
+            for(int j=i;j<n;j++){
+                if(!hs.contains(s.charAt(j))){
+                    hs.add(s.charAt(j));
+                }
+                else {
+                    break;
+                }
+            }
+            maxi = Math.max(maxi, hs.size());
+            }
+        return maxi;
+    }
+}
