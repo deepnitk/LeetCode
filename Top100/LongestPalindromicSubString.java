8406d04b85f5e647ecbd969325a59bc36d2e1563
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-04-29 08:42:41 +0530

    Update LongestPalindromicSubString.java

--- Top100/LongestPalindromicSubString.java
+++ Top100/LongestPalindromicSubString.java
@@ -33,3 +33,37 @@ class Solution {
         return true;
     }
 }
+
+//Better solution
+//TC:O(N2)
+class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        if(n==0) return "";
+        StringBuilder longestPalin = new StringBuilder();
+        for(int i=0;i<n;i++){
+            StringBuilder sb = new StringBuilder();
+            for(int j=i;j<n;j++){
+                sb.append(s.charAt(j));
+                if(isPalindrome(sb.toString())){                
+                    if(longestPalin.toString().length()<sb.toString().length())
+                            longestPalin=sb;     
+                }
+
+            }
+        }
+        return longestPalin.toString();
+    }
+    
+    private boolean isPalindrome(String s){
+        int n = s.length();
+        int left=0, right=n-1;
+        while(left<=right){
+            if(s.charAt(left)!=s.charAt(right))
+                return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
