88a0e4fc3ca816c74e4969c320722368dc973221
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-04-30 17:38:57 +0530

    Update LongestPalindromicSubString.java

--- Top100/LongestPalindromicSubString.java
+++ Top100/LongestPalindromicSubString.java
@@ -36,34 +36,38 @@ class Solution {
 
 //Better solution
 //TC:O(N2)
+//SC:O(1)
 class Solution {
     public String longestPalindrome(String s) {
+        //s = defabbadjk"
         int n = s.length();
-        if(n==0) return "";
-        StringBuilder longestPalin = new StringBuilder();
+        int start = 0;
+        int end = 0;
         for(int i=0;i<n;i++){
-            StringBuilder sb = new StringBuilder();
-            for(int j=i;j<n;j++){
-                sb.append(s.charAt(j));
-                if(isPalindrome(sb.toString())){                
-                    if(longestPalin.toString().length()<sb.toString().length())
-                            longestPalin=sb;     
-                }
-
+            int len1 = expandFromMiddle(s, i, i);
+            int len2 = expandFromMiddle(s, i,i+1);
+            int len = Math.max(len1, len2);
+            
+            if (len>end-start){
+                start = i -((len-1)/2);
+                end = i + (len/2);
             }
         }
-        return longestPalin.toString();
+        return s.substring(start, end+1);
     }
     
-    private boolean isPalindrome(String s){
-        int n = s.length();
-        int left=0, right=n-1;
-        while(left<=right){
-            if(s.charAt(left)!=s.charAt(right))
-                return false;
-            left++;
-            right--;
+    public int expandFromMiddle(String s, int left, int right){
+        if (s == null || left> right) {
+            return 0;
         }
-        return true;
+        while(left>=0 && right< s.length()
+             && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+        }
+        return right - left -1;
+        
     }
 }
+
+
