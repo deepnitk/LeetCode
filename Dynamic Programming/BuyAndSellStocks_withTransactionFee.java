f394e240f3784919dd35d9b1a440687a0c31f9bd
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-05-02 16:32:38 +0530

    Update BuyAndSellStocks_withTransactionFee.java

--- Dynamic Programming/BuyAndSellStocks_withTransactionFee.java
+++ Dynamic Programming/BuyAndSellStocks_withTransactionFee.java
@@ -21,3 +21,35 @@ class Solution {
         return profit;
     }
 }
+
+//Tabulation solution
+
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        return (int)maxProfitUtils(prices, fee);
+    }
+    public int maxProfitUtils(int[] prices, int fee) {
+        int n = prices.length;
+        int[][] dp = new int[n+1][2];
+        for(int[] row:dp)
+            Arrays.fill(row, 0);
+        //Base case
+        dp[n][0] = dp[n][1] = 0;
+        
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=1;j++){
+                long profit = 0;
+                if(j == 1){
+                    profit = Math.max(-prices[i]+dp[i+1][0],
+                                     0 + dp[i+1][1]);
+                }
+                else{
+                    profit = Math.max(prices[i] + dp[i+1][1] -fee ,
+                                     0 + dp[i+1][0]);
+                }
+                dp[i][j] = (int)profit;
+            }
+        }
+        return dp[0][1];
+    }
+}
