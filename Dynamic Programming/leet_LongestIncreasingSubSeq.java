62292b37720fc389e03ea0b0e141d6163706e4c2
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-05-03 12:15:11 +0530

    Update leet_LongestIncreasingSubSeq.java

--- Dynamic Programming/leet_LongestIncreasingSubSeq.java
+++ Dynamic Programming/leet_LongestIncreasingSubSeq.java
@@ -88,3 +88,34 @@ class Solution {
         return dp[0][-1+1];
     }
 }
+
+//Space Optimization
+//TC:O(N2)
+//SC:O(N)
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] nextRow = new int[n+1];
+        Arrays.fill(nextRow, 0);
+        int[] prevRow = new int[n+1];
+        Arrays.fill(prevRow, 0);        
+        
+        for (int idx = n-1;idx>=0;idx--) {
+            for(int prevIdx = idx-1; prevIdx>=-1;prevIdx--) {
+                int pick = 0;
+                int notPick = 0;
+                if (prevIdx == -1 || nums[idx] > nums[prevIdx]) {
+                    //co-ordinate shift hence 2nd paramaeter+1
+                    pick = 1 + nextRow[idx+1];
+                }
+                //co-ordinate shift hence 2nd paramaeter+1
+                notPick = 0 + nextRow[prevIdx+1];
+
+                prevRow[prevIdx+1] = Math.max(pick, notPick);                    
+            }
+            nextRow = prevRow;
+        }
+        
+        return nextRow[-1+1];
+    }
+}
