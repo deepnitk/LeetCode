c3882448a3e7a77d666a58661770369424b4ea0d
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-05-04 08:35:50 +0530

    Update Leet100_3Sum.java

--- Top100/Leet100_3Sum.java
+++ Top100/Leet100_3Sum.java
@@ -41,3 +41,44 @@ class Solution {
         }
     }
 }
+
+//Optimal solution
+//TC:O(N2)
+//SC:ASA of O(1)
+class Solution {
+    
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res  = new ArrayList<List<Integer>>();
+        for(int i = 0; i<nums.length-2; i++){
+            if(i==0 || nums[i]!=nums[i-1]){
+                int lo = i+1;
+                int hi = nums.length - 1;
+                int sum = 0 - nums[i];
+                while(lo < hi) {
+                    if (nums[lo]+nums[hi] == sum){
+                        res.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+
+                        while(lo < hi && nums[lo] == nums[lo+1]) {
+                            lo++;
+                        }
+                        while(lo < hi && nums[hi] == nums[hi-1]) {
+                            hi--;
+                        }
+                        lo++;
+                        hi--;
+                    }
+                    else if (nums[lo]+nums[hi] < sum) {
+                        lo++;
+                    }
+                    else {
+                        hi--;
+                    }                    
+                }
+
+            }
+        }
+        return res;
+    }
+}
