6fd7becde079fc4ad028c169a93943947d009412
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-05-12 08:29:15 +0530

    Update LongestIncreasingSubSeq.java

--- Dynamic Programming/LongestIncreasingSubSeq.java
+++ Dynamic Programming/LongestIncreasingSubSeq.java
@@ -31,6 +31,31 @@ class Solution {
     }
 }
 
+//Tabulation 
+
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n+1][n+1];
+        
+        for(int[] row:dp){
+            Arrays.fill(row, 0);
+        }
+        
+        for(int idx = n-1;idx>=0;idx--) {
+            for(int prev_idx = idx-1;prev_idx>=-1;prev_idx--){
+                int len = 0 + dp[idx+1][prev_idx + 1];
+        
+                if(prev_idx == -1 || nums[idx] > nums[prev_idx]) {
+                    len = Math.max(len, 1 +dp[idx+1][idx+1]);
+                }
+                dp[idx][prev_idx+1] = len;
+            }
+        }
+        return dp[0][-1+1];
+    }
+}
+
 
 //Tabulation solution
 //TC:O(N^2)
