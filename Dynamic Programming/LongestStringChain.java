587354165f0546034d9c6e3d9c9f49c74fa5d58a
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-05-12 18:18:30 +0530

    Update LongestStringChain.java

--- Dynamic Programming/LongestStringChain.java
+++ Dynamic Programming/LongestStringChain.java
@@ -17,7 +17,7 @@ class Solution {
         
         for(int idx = 0; idx<n;idx++) {
             for(int prev_idx = 0; prev_idx<idx; prev_idx++){
-                if (compare(words[prev_idx], words[idx]) && 1+dp[prev_idx] > dp[idx]) {
+                if (compare(words[idx],words[prev_idx]) && 1+dp[prev_idx] > dp[idx]) {
                    dp[idx] = Math.max(1+dp[prev_idx], dp[idx]);
                 }
             }
@@ -29,31 +29,30 @@ class Solution {
         return maxLen;        
     }
     
-    public boolean compare(String s1, String s2) {
-        int l1 = s1.length();
-        int l2 = s2.length();
-        if (Math.abs(l2 - l1) != 1) {
+    public boolean compare(String curr, String prev) {
+        int currLen = curr.length();
+        int prevLen = prev.length();
+        
+        if(currLen != prevLen +1) {
             return false;
         }
-        int i = 0;
-        int j = 0;
-        int misMatchCount = 0;
-        while(i<l1 && j<l2) {
-            char c1 = s1.charAt(i);
-            char c2 = s2.charAt(j);
-            if(c1 == c2) {
-                i++;
-                j++;
+        
+        int first = 0; 
+        int second = 0;
+        while(first < currLen){
+            if(second < prevLen && curr.charAt(first) == prev.charAt(second)) {
+                first++;
+                second++;
             }
             else {
-                j++;
-                misMatchCount++;
-                if(misMatchCount > 1) {
-                    return false;
-                }
+                first++;
             }
         }
-        return true;
+        
+        if(first == currLen && second == prevLen) {
+            return true;
+        }
+        return false;
     }
     
                 
