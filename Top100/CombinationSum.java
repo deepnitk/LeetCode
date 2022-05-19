78f604d47daf112d0fbac323039a1bf9928d2021
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-05-19 11:48:12 +0530

    Create CombinationSum.java
    Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
    
    The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
    
    It is guaranteed that the number of unique combinations that sum up to target is less than 150 combinations for the given input.

---
+++ Top100/CombinationSum.java
@@ -0,0 +1,32 @@
+//Recursive solution
+//TC: o(2^t)*k
+
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        combinationSumUtils(0, candidates, target, res, new ArrayList<>());
+        return res;
+    }
+    
+    public void combinationSumUtils(int idx, int[] arr, int target, List<List<Integer>> res, List<Integer> ds){
+        // Base case
+        if(idx == arr.length){
+            if(target == 0){
+                res.add(new ArrayList<>(ds));
+            }
+            return;
+        }
+        
+        //Pick case
+        if (arr[idx] <= target){
+            ds.add(arr[idx]);
+            combinationSumUtils(idx, arr, target-arr[idx], res, ds);
+            ds.remove(ds.size()-1);
+        }
+        
+        //not pick case
+        combinationSumUtils(idx+1, arr, target, res, ds);
+        
+    }
+}
