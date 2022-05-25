7f2d6517bc79696572061e9eecb2d3ea220dd362
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-05-25 21:06:07 +0530

    Update MergeKSortedLists.java

--- GogglePractice/MergeKSortedLists.java
+++ GogglePractice/MergeKSortedLists.java
@@ -30,36 +30,13 @@ class Solution {
         }
 
         //Merge h1 and h2 to head
-        //return mergeTwoSortedList(h1, h2);
-        //Merge h1 and h2 into head
-        if(h1 == null && h2 == null) //No list is present
-            return head;
-        else if(h1 == null)    //Only 2nd list is present
-            return h2;  
-        else if(h2 == null)    //Only 1st list is present
-            return h1;
-        
-        if(h1 == null || (h1 != null && h1.val>h2.val))
-        {   head = h2;  h2=h2.next;    }
-        else
-        {   head = h1;  h1=h1.next;    }
-        
-        ptr = head;
-        while(h1 != null || h2 != null)
-        {
-            if(h1 == null)
-            {   ptr.next = h2;   h2=h2.next;  ptr=ptr.next;  }
-            else if(h2 == null)
-            {   ptr.next = h1;   h1=h1.next;  ptr=ptr.next;  }
-            else if(h1.val < h2.val)
-            {   ptr.next=h1;   h1=h1.next;    ptr=ptr.next;  }
-            else
-            {   ptr.next=h2;   h2=h2.next;    ptr=ptr.next;  }
-        }
-        return head;
+        return mergeTwoSortedList(h1, h2);
     }
 
     public ListNode mergeTwoSortedList(ListNode l1 , ListNode l2) {
+        if(l1 == null && l2 == null) {
+            return null;
+        }
         if (l1 == null) return l2;
         if (l2 == null) return l1;
         if(l1.val > l2.val) {
@@ -77,7 +54,7 @@ class Solution {
             }
             tmp.next = l2;
             // Swap
-            ListNode temp = l2;
+            ListNode temp = l1;
             l1 = l2;
             l2 = temp;
         }
@@ -96,33 +73,7 @@ class Solution {
         h1 = merge(lists, left, left+k/2-1);
         h2 = merge(lists, left + k/2, right);
 
-        // return mergeTwoSortedList(h1, h2);
-                //Merge h1 and h2 into head
-        if(h1 == null && h2 == null) //No list is present
-            return head;
-        else if(h1 == null)    //Only 2nd list is present
-            return h2;  
-        else if(h2 == null)    //Only 1st list is present
-            return h1;
-        
-        if(h1 == null || (h1 != null && h1.val>h2.val))
-        {   head = h2;  h2=h2.next;    }
-        else
-        {   head = h1;  h1=h1.next;    }
-        
-        ptr = head;
-        while(h1 != null || h2 != null)
-        {
-            if(h1 == null)
-            {   ptr.next = h2;   h2=h2.next;  ptr=ptr.next;  }
-            else if(h2 == null)
-            {   ptr.next = h1;   h1=h1.next;  ptr=ptr.next;  }
-            else if(h1.val < h2.val)
-            {   ptr.next=h1;   h1=h1.next;    ptr=ptr.next;  }
-            else
-            {   ptr.next=h2;   h2=h2.next;    ptr=ptr.next;  }
-        }
-        return head;
-
+        return mergeTwoSortedList(h1, h2);
     }
 }
+
