e0a9f2f50a9d512f94c2bcab3cb6262eb90ca3b7
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-05-26 20:43:39 +0530

    Create TrappingRainWater.java
    Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.

---
+++ GogglePractice/TrappingRainWater.java
@@ -0,0 +1,34 @@
+//TC:O(N)
+//SC:o(1)
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int leftMax = 0;
+        int rightMax = 0;
+        int leftPtr = 0;
+        int rightPtr = n-1;
+        int trappedWater = 0;
+        while(leftPtr < rightPtr) {
+            if(height[leftPtr] <= height[rightPtr]){
+                if(height[leftPtr] >= leftMax){
+                    leftMax = height[leftPtr];
+                }
+                else {
+                    trappedWater += leftMax - height[leftPtr];
+                }
+                leftPtr++;
+            }
+            else {
+                if(height[rightPtr] >= rightMax){
+                    rightMax = height[rightPtr];
+                }
+                else {
+                    trappedWater += rightMax - height[rightPtr];
+                }
+                rightPtr--;
+            }
+        }
+        
+        return trappedWater;
+    }
+}
