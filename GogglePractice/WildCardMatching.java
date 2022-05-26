16d4a3e205d0d106be5972509866591da55945d9
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-05-26 21:50:12 +0530

    Update WildCardMatching.java

--- GogglePractice/WildCardMatching.java
+++ GogglePractice/WildCardMatching.java
@@ -62,3 +62,41 @@ class Solution {
 		return dp[i][j] = false;
 	}
 }
+
+//Tabulation solution
+//TC: O(N*M)
+//SC: O(N*M)
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int n  = s.length();
+        int m = p.length();
+        Boolean[][] dp = new Boolean[n+1][m+1];
+        dp[0][0] = true;
+        for(int i=1;i<=n;i++) {
+            dp[i][0] = false;
+        }
+        
+        for(int j=1;j<=m;j++){
+            boolean flag = true;
+            for(int k=1;k<=j;k++){
+                if(p.charAt(k-1) != '*') {
+                    flag = false;
+                    break;
+                }
+            }
+            dp[0][j] = flag;  
+        }
+        for(int i = 1;i<=n;i++) {
+            for(int j =1; j <= m; j++){
+                if (s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else if (p.charAt(j-1) == '*') {
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                }
+                else dp[i][j] = false;                
+            }
+        }
+        return dp[n][m];
+    }
+}
