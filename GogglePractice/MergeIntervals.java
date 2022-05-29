6b87a7766c5e678b1f03f2df915a6de5da579fb3
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-05-29 12:02:03 +0530

    Create MergeIntervals.java
    Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.

---
+++ GogglePractice/MergeIntervals.java
@@ -0,0 +1,31 @@
+//TC: O(N)
+//SC: O(N)
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Stack<int[]> st = new Stack();
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        st.push(intervals[0]);
+        for(int i = 1; i<intervals.length ; i++) {
+            int[] top = st.peek();
+	
+           // Overlapping interval
+            if (top[1] >= intervals[i][0]) {
+                st.pop();
+                top[1] = Math.max(top[1], intervals[i][1]);
+                st.push(top);
+            }
+            else {
+                st.push(intervals[i]);
+            }
+        }
+        int[][] res = new int[st.size()][2];
+        int i = 0;
+        while(!st.isEmpty()) {
+            int[] top = st.pop();
+            res[i][0] = top[0];
+            res[i][1] = top[1];
+            i++;
+        }
+         return res;
+    }
+}
