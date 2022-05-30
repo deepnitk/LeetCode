6afff3090214ef9de58b8503fdb53e44136cafc8
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-05-30 13:45:11 +0530

    Update WordBreak.java

--- GogglePractice/WordBreak.java
+++ GogglePractice/WordBreak.java
@@ -50,3 +50,25 @@ class Solution {
     	}
 }
 
+
+//DP solution
+class Solution {
+	public boolean wordBreak(String s, List<String> wordDict) {
+		int n = s.length();
+		HashSet<String> hs = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[n+1];
+        dp[n] = true;
+        for(int i = n-1;i >= 0;i--){
+            for(String w: wordDict) {
+                if((i + w.length() <= s.length()) &&  w.equals(s.substring(i, i + w.length()))) {
+                    dp[i] = dp[i + w.length()];
+                }
+                if(dp[i]) {
+                    break;
+                }
+            }
+        }
+		return dp[0];
+	}
+}
+
