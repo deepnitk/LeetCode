a340c904a40711b76af7e4d6c35bea1223cdbb15
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-05-30 10:28:52 +0530

    Update LongestConsecutiveSequence.java

--- GogglePractice/LongestConsecutiveSequence.java
+++ GogglePractice/LongestConsecutiveSequence.java
@@ -3,15 +3,12 @@
 class Solution {
 	public int longestConsecutive(int[] nums) {
 		HashSet<Integer> hs = new HashSet<>();
-        //Base case
-        if (nums.length == 0 || nums == null) {
-            return 0;
-        }
+
         //Add every element to hashset
 		for(int num : nums) {
 			hs.add( num);
 		}
-		int maxLen = 1;
+		int maxLen = 0;
         //Iterate again. if (nums[i] - 1) exists continue, elif (nums[i] + 1) increase maxLen by 1 until u get it.
 		for(int i =0; i < nums.length; i++) {
 			if (!hs.contains(nums[i] - 1)) {
