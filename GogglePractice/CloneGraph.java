cb599a74a7be2c6d35b0ce816bea27edbeff6bb1
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-05-30 11:49:01 +0530

    Create CloneGraph.java
    Given a reference of a node in a connected undirected graph.
    
    Return a deep copy (clone) of the graph.
    
    Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
    
    class Node {
        public int val;
        public List<Node> neighbors;
    }

---
+++ GogglePractice/CloneGraph.java
@@ -0,0 +1,47 @@
+//TC:O(V+E)
+//SC:O(V+E)
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+	public Node cloneGraph(Node node) {
+		Map<Node, Node> visited = new HashMap<>();
+        return clone(node, visited);
+
+	}
+	public Node clone(Node node, Map<Node, Node> visited) {
+		if (node == null) {
+			return node;
+		}
+
+		if (visited.containsKey(node)) {
+			return visited.get(node);
+		}
+		
+		Node cloneNode = new Node(node.val, new ArrayList<>());
+		visited.put(node, cloneNode);
+		
+		for(Node n : node.neighbors) {
+			cloneNode.neighbors.add(clone(n, visited));
+		}
+		
+		return cloneNode;
+	}
+}
