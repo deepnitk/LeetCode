788f2cb012852cd2ac33c86dd09b94fe6d836463
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-05-30 12:03:41 +0530

    Update CloneGraph.java

--- GogglePractice/CloneGraph.java
+++ GogglePractice/CloneGraph.java
@@ -46,3 +46,52 @@ class Solution {
 		return cloneNode;
 	}
 }
+
+//BFS solution
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+	public Node cloneGraph(Node node) {
+		Map<Node, Node> visited = new HashMap<>();
+        if (node == null) {
+            return node;
+        }
+        Queue<Node> q  = new LinkedList<>();
+        q.add(node);
+        visited.put(node, new Node(node.val, new ArrayList<>()));
+        
+        while (!q.isEmpty()) {
+            Node popped = q.poll();
+            
+            for(Node n : popped.neighbors) {
+                if (!visited.containsKey(n)) {
+                    visited.put(n, new Node(n.val, new ArrayList<>()));
+                    q.add(n);
+                }
+                visited.get(popped).neighbors.add(visited.get(n));
+            }
+        }
+        return visited.get(node);
+
+	}
+}
+
