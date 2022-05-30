fed57b3b73960932ba545634700d9fb6c29ac343
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-05-30 13:35:16 +0530

    Update WordBreak.java

--- GogglePractice/WordBreak.java
+++ GogglePractice/WordBreak.java
@@ -20,3 +20,33 @@ class Solution {
         return false;
     }
 }
+
+//Memoization solution
+
+class Solution {
+	public boolean wordBreak(String s, List<String> wordDict) {
+		int n = s.length();
+		HashSet<String> hs = new HashSet<>(wordDict);
+		int[] dp = new int[n];
+		Arrays.fill(dp, -1);
+			return wordBreakUtils(0, n, s, hs, dp);
+		}
+
+	public boolean wordBreakUtils(int pos, int n, String s, HashSet<String> hs, int[] dp) {
+		if (pos == n) {
+		    return true;
+		}
+		if (dp[pos] != -1) {
+		    return dp[pos] == 1? true: false;
+		}
+		for(int i = pos+1; i <= n;i++) {
+		    if(hs.contains(s.substring(pos, i)) && wordBreakUtils(i, n, s, hs, dp)) {
+			dp[pos] = 1;
+			return true;
+		    }
+		}
+		dp[pos] = 0;
+		return false;
+    	}
+}
+
