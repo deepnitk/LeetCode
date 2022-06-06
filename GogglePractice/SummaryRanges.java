115bc55a30108ea9f7f00abc72ff519067b47a17
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-06-06 17:04:07 +0530

    Update SummaryRanges.java

--- GogglePractice/SummaryRanges.java
+++ GogglePractice/SummaryRanges.java
@@ -2,31 +2,28 @@ class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> res = new ArrayList<>();
         
-        int start = 0;
         int i = 0;
         int n = nums.length;
         
-        while (i < n - 1 ) {
-            start = i;
-            while (i< n - 1 && nums[i] == nums[i+1] -1) {
-                i++;
+        while (i < n) {
+            int start = i;
+            int end = i;
+            while (end + 1 < n && nums[end + 1] == nums[end] + 1) {
+                end++;
             }
-            if (nums[start] != nums[i]){
+            if (end > start){
                 StringBuilder s = new StringBuilder();
                 s.append(String.valueOf(nums[start]));
                 s.append("->");
-                s.append(String.valueOf(nums[i]));
+                s.append(String.valueOf(nums[end]));
                 res.add(s.toString());
-                i++;
             }
             else {
-                res.add(String.valueOf(nums[i]));
-                i++;
+                res.add(String.valueOf(nums[start]));
             }
+            i = end + 1;
         }
-        if ( i != n) {
-            res.add(String.valueOf(nums[i]));
-        }
+
         return res;
     }
 }
