dbab2ce335bee28921e13810b6386d7c969b56de
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-06-09 08:06:25 +0530

    Update KthSmallestInBST.java

--- GogglePractice/KthSmallestInBST.java
+++ GogglePractice/KthSmallestInBST.java
@@ -30,3 +30,44 @@ class Solution {
         constructPreOrder(root.right, inOrder);
     }
 }
+
+//Better Approach : without storing inOrder traversal
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+
+        TreeNode smallest = constructPreOrder(root, new int[]{k});
+        if( smallest != null)
+            return smallest.val;
+        return -1;
+    }
+    
+    TreeNode constructPreOrder(TreeNode root, int[] k) {
+        if(root == null) {
+            return null;
+        }
+        TreeNode left = constructPreOrder(root.left, k);
+        if (left != null) {
+            return left;
+        }
+
+        k[0]--;
+        if (k[0] == 0) 
+            return root;
+        return constructPreOrder(root.right, k);
+    }
+}
