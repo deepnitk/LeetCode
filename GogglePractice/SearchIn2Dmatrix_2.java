47a0050c8f5684ada3f6ebb27fb9907150b034b5
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-06-09 21:35:44 +0530

    Create SearchIn2Dmatrix_2.java
    Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
    
    Integers in each row are sorted from left to right.
    The first integer of each row is greater than the last integer of the previous row.

---
+++ GogglePractice/SearchIn2Dmatrix_2.java
@@ -0,0 +1,27 @@
+//Awesome case 
+//represent 2d array in 1d fashion like res = (0 to m*n - 1)
+// r will be res/m and c will be res%n
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        
+        int lo  = 0;
+        int hi = (m * n) - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo)/2;
+            int r = mid / m;
+            int c = mid % m;
+            if (matrix[r][c] == target) {
+                return true;
+            }
+            else if (matrix[r][c] > target) {
+                hi = mid - 1;
+            }
+            else {
+                lo = mid + 1;
+            }
+        }
+        return false;
+    }
+}
