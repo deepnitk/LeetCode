eeeebcbb4a272fefc96069edd5a762865a370100
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-06-10 17:53:19 +0530

    Update PerfectSquares.java

--- GogglePractice/PerfectSquares.java
+++ GogglePractice/PerfectSquares.java
@@ -14,3 +14,26 @@ class Solution {
         return ans;
     }
 }
+
+//Memoization solution
+//tc: o(n*sqrt(n))
+class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return solve(n, dp);
+    }
+    int solve(int n, int[] dp) {
+        if (n <= 3) {
+            return n;
+        }
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+        int ans = n;
+        for(int i = 1; i*i <= n; i++){
+            ans = Math.min(ans, 1 + solve(n - i*i, dp));
+        }
+        return dp[n] = ans;
+    }
+}
