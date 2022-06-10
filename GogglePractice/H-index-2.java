866dcb3d27e85f771698aa35185256a9bd316596
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-06-10 13:19:02 +0530

    Update H-index-2.java

--- GogglePractice/H-index-2.java
+++ GogglePractice/H-index-2.java
@@ -10,3 +10,28 @@ class Solution {
         return res - 1;
     }
 }
+
+//Optimal Solution
+//TC: O(logn)
+
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int lo = 0;
+        int hi = n - 1;
+
+        while ( lo <= hi ) {
+            int mid = lo + (hi - lo)/2;
+            if (citations[mid] == n - mid){
+                return citations[mid];
+            }
+            else if (citations[mid] > n - mid){
+                hi = mid - 1;
+            }
+            else {
+                lo = mid + 1;
+            }
+        }
+        return n - lo;
+    }
+}
