cd489eb5d64d502d02d9dc3f3827f91f9cebd2be
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-06-13 08:13:22 +0530

    Update Subsets.java

--- Recursion/Subsets.java
+++ Recursion/Subsets.java
@@ -20,3 +20,33 @@ class Solution {
         return res;
     }
 }
+
+//Recursive solution
+
+// Bit Manipulation Approach
+// TC: O(2^n * n)
+// SC : O(1)
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+        for(int k = 0; k <= n; k++) {
+            backTrack(0, k, n, nums, new ArrayList<>(), res);
+        }
+        return res;
+    }
+    
+    private void backTrack( int idx, int k, int n, int[] nums, ArrayList<Integer> curr, List<List<Integer>> res) {
+        if ( k == curr.size() ) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        
+        for(int i = idx; i < n; i++) {
+            curr.add(nums[i]);
+            backTrack(i + 1, k, n, nums, curr, res);
+            curr.remove(curr.size() - 1);
+        }
+        
+    }
+}
