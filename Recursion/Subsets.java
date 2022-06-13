089ecf82c70f3dd961589ad2525b2f10e28705ad
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-06-13 09:04:19 +0530

    Update Subsets.java

--- Recursion/Subsets.java
+++ Recursion/Subsets.java
@@ -26,27 +26,30 @@ class Solution {
 // Bit Manipulation Approach
 // TC: O(2^n * n)
 // SC : O(1)
+// Bit Manipulation Approach
+// TC: O(2^n * n)
+// SC : O(1)
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        int n = nums.length;
-        for(int k = 0; k <= n; k++) {
-            backTrack(0, k, n, nums, new ArrayList<>(), res);
-        }
-        return res;
+		int n = nums.length;
+		subSetsUtils(0, n, nums, new ArrayList<>(), res);
+		return res;
     }
     
-    private void backTrack( int idx, int k, int n, int[] nums, ArrayList<Integer> curr, List<List<Integer>> res) {
-        if ( k == curr.size() ) {
-            res.add(new ArrayList<>(curr));
+    private static void subSetsUtils(int idx, int n, int[] nums, List<Integer> subSet, List<List<Integer>> res) {
+        if ( idx == n ) {
+            res.add(new ArrayList<>(subSet));
             return;
         }
-        
-        for(int i = idx; i < n; i++) {
-            curr.add(nums[i]);
-            backTrack(i + 1, k, n, nums, curr, res);
-            curr.remove(curr.size() - 1);
-        }
-        
+
+        //Pick case
+        subSet.add(nums[idx]);
+        subSetsUtils(idx + 1, n, nums, subSet, res);
+        subSet.remove(subSet.size() - 1);
+
+        //Not pick case
+        subSetsUtils(idx + 1, n, nums, subSet, res);
     }
+
 }
