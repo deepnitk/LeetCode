fd8724746fb3ade6fbacd3f17c1970df7c37068a
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-06-14 09:08:50 +0530

    Update Permutation.java

--- Recursion/Permutation.java
+++ Recursion/Permutation.java
@@ -51,3 +51,39 @@ class Solution {
         }
     }
 }
+
+//Better approach
+//TC: O(N! * N)
+//sc: ASA
+
+
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        permuteUtils(0, nums, res);
+        return res;
+    }
+    
+    private void permuteUtils(int idx, int[] nums, List<List<Integer>> res) {
+        if ( idx == nums.length) {
+            List<Integer> ds = new ArrayList<>();
+            for(int el: nums) {
+                ds.add(el);
+            }
+            res.add(new ArrayList<>(ds));
+            return;
+        }
+        
+        for (int i = idx; i < nums.length; i++) {
+            swap(i, idx, nums);
+            permuteUtils(idx+1, nums, res);
+            swap(i, idx, nums);
+        }
+    }
+    
+    private void swap(int i, int j, int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
