ff37e7473b8665f3d64fa127b72d4bad4ed9d402
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-06-15 13:03:05 +0530

    Create SudokuSolver.java

---
+++ Recursion/SudokuSolver.java
@@ -0,0 +1,47 @@
+/* 
+Time Complexity: O(9(n ^ 2)), in the worst case, for each cell in the n2 board, we have 9 possible numbers.
+Space Complexity: O(1), since we are refilling the given board itself, there is no extra space required, so constant space complexity.
+*/
+
+class Solution {
+    public void solveSudoku(char[][] board) {
+            solve(board);
+    }
+    
+    private boolean solve(char[][] board) {
+        for (int i = 0;i < 9;i++) {
+            for (int j = 0;j < 9; j++) {
+                if ( board[i][j] == '.') {
+                    for (char c = '1'; c <= '9';c++) {
+                        if ( isValid(board, i, j, c) ) {
+                            board[i][j] = c;
+                            if ( solve(board) ) {
+                                return true;
+                            }
+                            else {
+                                board[i][j] = '.';
+                            }
+                        } 
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    private boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == c) {
+                return false;
+            }
+            if (board[row][i] == c) {
+                return false;
+            }
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
