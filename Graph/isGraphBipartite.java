16edc8a0b0c39fd87d17e21a15d21d642a1f5b0e
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-06-21 18:56:48 +0530

    Update isGraphBipartite.java

--- Graph/isGraphBipartite.java
+++ Graph/isGraphBipartite.java
@@ -2,6 +2,7 @@
 
 Space Complexity: O(V), because, apart from the graph, we maintain a color array. */
 
+// DFS Solution
 class Solution {
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
@@ -30,5 +31,42 @@ class Solution {
             }
         }
         return true;     
+    }
+}
+
+//BFS solution
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int color[] = new int[n];
+        Arrays.fill(color, -1);
+        for(int i = 0;i < n; i++){
+            if (color[i] == -1) {
+                if (!isBipartiteUtils(graph, i, color)) {
+                    return false;
+                }
+            }
+        } 
+        return true;     
+    }
+    
+    private boolean isBipartiteUtils(int[][] graph, int nde, int[] color) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(nde);
+        color[nde] = 1;
+        
+        while ( !q.isEmpty() ) {
+            int node = q.poll();
+            for(int next : graph[node]) {
+                if ( color[next] == -1) {
+                    q.add(next);
+                    color[next] = 1 - color[node];
+                }
+                else if ( color[next] == color[node]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
