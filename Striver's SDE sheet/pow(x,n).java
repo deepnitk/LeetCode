c2378584f019dd3b889bac736c5be4c6d16b0af4
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-07-14 19:47:35 +0530

    Create pow(x,n).java
    Implement pow(x, n), which calculates x raised to the power n (i.e., xn).

---
+++ Striver's SDE sheet/pow(x,n).java
@@ -0,0 +1,19 @@
+//Brute force
+// TC: O(n)
+class Solution {
+    public double myPow(double x, int n) {
+        double ans = 1.0;
+        long nn = n;
+        if (nn < 0) {
+            nn = -1 * nn;
+        }
+        while (nn > 0) {
+            ans = ans*x;
+            nn -= 1;
+        }
+        if (n < 0) {
+            ans = (double)1.0/(double)ans;
+        }
+        return ans;
+    }
+}
