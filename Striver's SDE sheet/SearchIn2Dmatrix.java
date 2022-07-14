75c44c127e8919a0cf145d64538ff322ee5015df
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-07-14 19:22:16 +0530

    Update SearchIn2Dmatrix.java

--- Striver's SDE sheet/SearchIn2Dmatrix.java
+++ Striver's SDE sheet/SearchIn2Dmatrix.java
@@ -23,3 +23,26 @@ class Solution {
         return false;
     }
 }
+
+// 2nd approach : work with row wise + column wise sorted as well
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int i = 0;
+        int j = n - 1;
+        while (i < m && j >= 0) {
+            if (matrix[i][j] == target)
+                return true;
+            else if (matrix[i][j] < target) {
+                i++;
+            }
+            else {
+                j--;
+            }
+        }
+        return false;
+    }
+}
