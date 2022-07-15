5b4f14526dc2e10eae8c1071dd7504726817b669
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-07-15 08:11:25 +0530

    Update MajorityElement.java

--- Striver's SDE sheet/MajorityElement.java
+++ Striver's SDE sheet/MajorityElement.java
@@ -1,5 +1,5 @@
 //Brute force
-// TC: O(N) SC: O(N)
+// TC: O(Nlogn) SC: O(N)
 class Solution {
     public int majorityElement(int[] nums) {
         HashMap<Integer, Integer> hm = new HashMap<>();
@@ -22,5 +22,27 @@ class Solution {
             }
         }
         return res;
+    }
+}
+
+//Optimal Approach
+//Moore's Voting algo
+// TC: O(N) SC:O(1)
+class Solution {
+    public int majorityElement(int[] nums) {
+        int cnt = 0;
+        int el = 0;
+        for(int i = 0; i < nums.length;i++) {
+            if(cnt == 0) {
+                el = nums[i];
+            }
+            if (el == nums[i]) {
+                cnt += 1;
+            }
+            else {
+                cnt -= 1;
+            }
+        }
+        return el;
     }
 }
