0a2fdf7254f25aff62bd2587f8abaa07486d4ec6
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-07-15 09:33:10 +0530

    Create UniquePaths.java
    There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
    
    Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
    
    The test cases are generated so that the answer will be less than or equal to 2 * 109.

---
+++ Striver's SDE sheet/UniquePaths.java
@@ -0,0 +1,19 @@
+//Brute force Recursive solution
+//TC: 
+
+class Solution {
+    public int uniquePaths(int m, int n) {
+        return uniquePathsUtils(0, 0, m, n);
+    }
+    
+    private int uniquePathsUtils(int i , int j, int m, int n) {
+        if( i == m - 1 && j == n - 1) {
+            return 1;
+        }
+        if ( i >= m || j >= n){
+            return 0;
+        }
+        
+        return uniquePathsUtils(i + 1, j, m, n) + uniquePathsUtils(i, j + 1, m, n);
+    }
+}
