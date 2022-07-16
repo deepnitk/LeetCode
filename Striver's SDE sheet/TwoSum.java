c36fcc67244cd3282963b4a9a4c32561b3a50a90
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-07-16 09:18:33 +0530

    Update TwoSum.java

--- Striver's SDE sheet/TwoSum.java
+++ Striver's SDE sheet/TwoSum.java
@@ -6,12 +6,12 @@ class Solution {
         int[] res = new int[2];
         HashMap<Integer, Integer> hm = new HashMap();
         for(int i = 0;i < nums.length; i++) {
-            if(hm.containsKey(nums[i])) {
-                res[0] = hm.get(nums[i]);
+            if(hm.containsKey(target - nums[i])) {
+                res[0] = hm.get(target - nums[i]);
                 res[1] = i;
             }
             else {
-                hm.put(target - nums[i], i);
+                hm.put(nums[i], i);
             }
         }
         return res;
