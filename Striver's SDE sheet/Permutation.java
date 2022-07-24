feed39c907331e63456ffeaf87086d195cc0e4f1
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-07-24 08:52:21 +0530

    Update Permutation.java

--- Striver's SDE sheet/Permutation.java
+++ Striver's SDE sheet/Permutation.java
@@ -27,3 +27,36 @@ class Solution {
         }
     }
 }
+
+//Optimal using swapping in place
+//tc: o(n!*n)
+//sc:o(1)
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        permuteUtils(0, nums, res);
+        return res;
+    }
+    
+    private void permuteUtils(int idx, int[] nums, List<List<Integer>> res) {
+        if (idx == nums.length) {
+            List<Integer> ds = new ArrayList<>();
+            for(int el : nums) {
+                ds.add(el);
+            }
+            res.add(new ArrayList<>(ds));
+            return;
+        }
+        
+        for (int i = idx; i < nums.length;i++) {
+            swap(nums, i, idx);
+            permuteUtils(idx + 1, nums, res);
+            swap(nums, i, idx);
+        }
+    }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
