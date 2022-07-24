293760f8473b5dbdd3e8d1b1c92cb47b2640bb9b
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-07-24 18:09:20 +0530

    Update ConvertBSTtoGreaterTree.java

--- Striver's SDE sheet/ConvertBSTtoGreaterTree.java
+++ Striver's SDE sheet/ConvertBSTtoGreaterTree.java
@@ -30,3 +30,42 @@ class Solution {
         convertBSTUtils(node.left);
     }
 }
+
+
+//Using static variable
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    static int sum;
+    static {
+        sum = 0;
+    }
+    public TreeNode convertBST(TreeNode root) {
+        sum = 0;
+        convertBSTUtils(root);
+        return root;
+    }
+    private void convertBSTUtils(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        
+        convertBSTUtils(node.right);
+        sum += node.val;
+        node.val = sum;
+        convertBSTUtils(node.left);
+    }
+}
